89195f3921a4b81f3c44ce8c5d3d1274e3e5e3d6
DIego Melo <diegomelodev@gmail.com>
2019-11-28 08:20:46 -0300

    create project

---
+++ src/main/java/com/involves/devtalks/DevtalksApplication.java
@@ -0,0 +1,13 @@
+package com.involves.devtalks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtalksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtalksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/involves/devtalks/DevtalksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.involves.devtalks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtalksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
